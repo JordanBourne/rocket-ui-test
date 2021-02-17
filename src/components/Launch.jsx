d12856d4b1b9de21e4f97716fe083cde019b0551
Jordan Bourne <jordan.bourne@billgo.com>
2021-02-16 22:22:47 -0700

    Got project running and implemented basic expanded info

--- src/components/Launch.jsx
+++ src/components/Launch.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Launch extends Component {
-
-  render() {
-
-    let launch = this.props.launch;
-
-    return (
-      <li>
-        <h2> { launch.mission_name } </h2>
-        <div> Flight Number: { launch.flight_number } </div>
-      </li>
-    );
+const Launch = ({ launch, expandedView, setExpandedView }) => {
+  const thisLaunchIsExpanded = expandedView === launch.flight_number;
+  const updateExpandedView = () => {
+    if (thisLaunchIsExpanded) {
+      setExpandedView('')
+    } else {
+      setExpandedView(launch.flight_number)
+    }
   }
-}
+
+  // implement curl --location --request GET 'https://api.spacexdata.com/v3/rockets' api to get the cost per launch and detail
+
+  return (
+    <li onClick={() => updateExpandedView()}>
+      <h2> { launch.mission_name } </h2>
+      <div> Flight Number: { launch.flight_number } </div>
+      {thisLaunchIsExpanded && (
+        <ul>
+          <li>Rocket ID: {launch.rocket.rocket_id}</li>
+          <li>Cost Per Launch: costPerLaunch</li>
+          <li>Description: description</li>
+        </ul>
+      )}
+    </li>
+  );
+};
 
 export default Launch;
--- src/views/Launches.jsx
+++ src/views/Launches.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import ConnectedView from './ConnectedView';
-import {fetchLaunchesIfNeeded} from "../actions/Launches";
+import { fetchLaunchesIfNeeded } from "../actions/Launches";
 import Launch from '../components/Launch';
 
-class LaunchesView extends Component {
-  componentDidMount() {
-    const { dispatch, launchesCollection } = this.props;
-    fetchLaunchesIfNeeded({ dispatch, launchesCollection });
-  }
+const LaunchesView = ({ dispatch, launchCollection }) => {
+  const [ expandedView, setExpandedView ] = useState('');
+  useEffect(() => {
+    fetchLaunchesIfNeeded({ dispatch, launchCollection });
+  }, []);
 
-  getContent() {
-    const { launchCollection } = this.props;
-
+  const getContent = () => {
     if (!launchCollection || launchCollection.fetching) {
       return <div> LOADING </div>;
     }
@@ -20,31 +18,26 @@ class LaunchesView extends Component {
       return <div> NO DATA </div>;
     }
 
-    let launches = [];
-
-    for (let i = 0; i < launchCollection.launches.length; i++) {
-      const launch = launchCollection.launches[i];
-
-      launches.push(
-        <Launch {...{
-          key: launch.launch_id,
-          launch
-        }} />
-
-      )
-    }
-
-    return <ul>{launches}</ul>;
-  }
-
-  render() {
     return (
-      <div>
-        <h2> SpaceX launches </h2>
-        {this.getContent()}
-      </div>
+      <ul>
+        {launchCollection.launches.map(launch => (
+          <Launch {...{
+            key: launch.launch_id,
+            launch,
+            expandedView,
+            setExpandedView
+          }} />
+        ))}
+      </ul>
     );
   }
-}
+
+  return (
+    <div>
+      <h2> SpaceX launches </h2>
+      {getContent()}
+    </div>
+  );
+};
 
 export default ConnectedView(LaunchesView, 'launches');
